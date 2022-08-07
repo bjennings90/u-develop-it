5b259175295be777243eb3c46bc96d81d60d16e7
bjennings90 <101131665+bjennings90@users.noreply.github.com>
2022-08-07 13:02:12 -0500

    add CRUD routes

--- server.js
+++ server.js
@@ -37,10 +37,6 @@ app.get('/api/candidates', (req, res) => {
     });
 });
 
-// db.query(`SELECT * FROM candidates`, (err, rows) => {
-//     console.log(rows);
-// });
-
 // GET a single candidate
 
 app.get('/api/candidate/:id', (req, res) => {
@@ -82,14 +78,14 @@ app.delete('/api/candidate/:id', (req, res) => {
 });
 
 // Create a candidate
-app.post('/api/candidate', (req, res) => {
+app.post('/api/candidate', ({ body }, res) => {
     const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
     if (errors) {
         res.status(400).json({ error: errors });
         return;
     }
-    const sql = `INSERT INTO candidates(id, first_name, last_name, industry_connected)
-                VALUES (?, ?, ?, ?)`;
+    const sql = `INSERT INTO candidates(first_name, last_name, industry_connected)
+                VALUES (?, ?, ?)`;
     const params = [body.first_name, body.last_name, body.industry_connected];
 
     db.query(sql, params, (err, result) => {
@@ -103,19 +99,6 @@ app.post('/api/candidate', (req, res) => {
         });
     });
 });
-
-// Create a candidate
-// const sql = `INSERT INTO candidates(id, first_name, last_name, industry_connected)
-//                 VALUES (?, ?, ?, ?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
-
-
-// db.query(sql, params, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
 
 //Default response for any other request (Not Found)
 app.use((req, res) => {
